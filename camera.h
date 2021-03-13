62888a65f4c38dab833cd7e18230e4716b4bab93
Denis Yevdokimov <55647933+denis-yevd@users.noreply.github.com>
2021-03-13 10:48:29 +0200

    Redone into h and cpp files instead classes.
    Made textures on walls and crosshair cursor.

--- camera.h
+++ camera.h
@@ -1,25 +1,1 @@
-#ifndef CAMERA_H
-#define CAMERA_H
-#include "point.h"
-
-class Camera
-{
-public:
-    float pi = 3.14159265359;
-    float angle_deg = 1; // in degrees
-    float angle_rad = 1.57; // in radians
-    float radius = 0.02;
-    Point pos = Point("position", 0, 0.3, 0);
-    Point view = Point("view", 0, 0.3, 0.02);
-    Point next_pnt;
-
-    Camera();
-    void MoveForward();
-    void MoveBack();
-    void TurnLeft();
-    void TurnRight();
-    void PrintPosition();
-    float ToRadians(float degree);
-};
-
-#endif // CAMERA_H
+#ifndef CAMERA_H#define CAMERA_H#include <glm/glm.hpp>using namespace glm;const float pi = 3.14159265359;extern float angle_deg; // in degreesextern float angle_rad; // in radiansextern float radius;extern vec3 pos;extern vec3 view;extern vec3 next_pnt;vec3 operator*(vec3 v, int d);    void MoveForward();    void MoveBack();    void TurnLeft();    void TurnRight();    void PrintPosition();#endif
